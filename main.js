c9090c6ddc0c25601bddd0394ae49fbccb41675c
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-07 15:24:24 -0400

    change functions on buttons

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ var currentQueArray = [];
 function onClick(object, value){
   console.log(object, value);
 }
+
+function print( num ){
+  console.log( 'The sum is: ' + num );
+}
+function calculate( nameOfCallbackFunction ){
+  let result = 4 * ( 35 / 7 );
+  nameOfCallbackFunction( result );
+}
+calculate( print );
