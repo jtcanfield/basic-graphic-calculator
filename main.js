8e9067a1d7976650c4ee80c67956afb46a895838
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-07 18:59:51 -0400

    add statement to cause calc to reset if number hit after calculating equation

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ var currentQueString = " ";
 var recentlyCalculated = false;
 var answerbox = document.getElementById("answer_box");
 function onClick(value, canReCalculate){
-  if (recentlyCalculated === true || canReCalculate === true){
-
+  if (recentlyCalculated === true && canReCalculate === false){
+    currentQueString = " ";
+    answerbox.textContent = " ";
   }
   currentQueString += ""+value+"";
   answerbox.textContent += value;
