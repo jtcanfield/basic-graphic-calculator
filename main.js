5057e590ae85a6dde621ecc96dbd84bb59e85dfd
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-07 15:47:56 -0400

    add values to answerbox

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 var currentQueString = " ";
+var answerbox = document.getElementById("answer_box");
 function onClick(object, value){
   console.log(value);
   currentQueString += ""+value+"";
   console.log(currentQueString);
+
 }
 function calculate( nameOfCallbackFunction ){
   console.log(eval(currentQueString));
-  let result = 4 * ( 35 / 7 );
+  let result = eval(currentQueString)
   nameOfCallbackFunction( result );
   // var currentQueString = "";
 }
 function print( num ){
-  var element = document.getElementById("answer_box");
-  element.textContent = num;
+  answerbox.textContent = num;
 }
