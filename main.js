aca56f20ba530cbe58f93028fd44ea1d32d9a840
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-07 16:03:28 -0400

    add que clear on calculate

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 var currentQueString = " ";
 var answerbox = document.getElementById("answer_box");
 function onClick(value){
+  console.log(currentQueString);
   currentQueString += ""+value+"";
   answerbox.textContent += value;
 }
 function calculate(){
   let result = eval(currentQueString);
   answerbox.textContent = result;
+  currentQueString = " ";
 }
 function onClickClear(){
   currentQueString = " ";
